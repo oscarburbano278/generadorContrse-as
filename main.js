9113c21aaa6b1dea7b447ccfc301de128210af75
oscar burbano <oscar10181@hotmail.com>
2024-09-25 13:11:35 -0500

    se implemento un boton para validar si la contraseña generada es segura o no

--- main.js
+++ main.js
@@ -9,39 +9,41 @@ const cadenaContrasena = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01
 
 function generar() {
     let numeroDigitado = parseInt (ingresoNum.value)
-    console.log(numeroDigitado);
+    //console.log(numeroDigitado);
     
     if (numeroDigitado < 8) {
         alert('la cantidad debe ser mayor o igual 8');
-    }
-
-    let password = '';
-    for (let i = 0; i < numeroDigitado; i++) {
-        let numAleatorio = cadenaContrasena [Math.floor(Math.random() * cadenaContrasena.length)];
-
-        //console.log(numAleatorio);
-
-        password += numAleatorio
         
-    }
-    contrasena.value = password;    
-    
-    function validarInput() {
-        // Obtén el valor del input
-        //const contraGenerada = document.getElementById('contrasena').value;
-    
-        // Expresión regular para validar:
-        // Al menos una letra minúscula, una letra mayúscula, un número y un carácter especial.
-        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
-    
-        // Valida si el valor cumple con la expresión regular
-        if (regex.test(password)) {
-           alert('exito')
-        } else {
-            alert('nooo')
+    }else{
+        let password = '';
+        for (let i = 0; i < numeroDigitado; i++) {
+            let numAleatorio = cadenaContrasena [Math.floor(Math.random() * cadenaContrasena.length)];
+
+            //console.log(numAleatorio);
+
+            password += numAleatorio
+            
         }
+        contrasena.value = password;
+    }    
+}
+
+function validarInput() {
+    // Obtén el valor del input
+    const contraGenerada = document.getElementById('contrasena').value;
+    console.log(contraGenerada);
+    // Expresión regular para validar:
+    // Al menos una letra minúscula, una letra mayúscula, un número y un carácter especial.
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+
+    // Valida si el valor cumple con la expresión regular
+    if (regex.test(contraGenerada)) {
+       alert('contraseña segura')
+    } else {
+        alert('contraseña NO segura ')
     }
 }
+
 
 function limpiar() {
     document.getElementById('contrasena').value = '';
